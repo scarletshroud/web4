85bfe2d40bebc9e53ddf5352858cf60435867e13
doomer888 <doomer888@protonmail.com>
2021-01-11 19:09:13 +0300

    utils

---
+++ src/main/java/slayer404/web4/utils/HitChecker.java
@@ -0,0 +1,19 @@
+package slayer404.web4.utils;
+
+public class HitChecker {
+    public static boolean isInArea(String firstVal, String secondVal, String thirdVal) {
+        try {
+
+            double x = Double.parseDouble(firstVal);
+            double y = Double.parseDouble(secondVal);
+            int r = Integer.parseInt(thirdVal);
+
+            return ((x > -r / 2.0 && x < 0 && y > -r && y < 0)
+                    || (x > 0 && y <= 0 && x * x + y * y < r / 2.0 * r / 2.0)
+                    || (x > 0 && y >= 0 && y < r / 2.0 - x));
+
+        } catch(NumberFormatException ex) {
+            return false;
+        }
+    }
+}
