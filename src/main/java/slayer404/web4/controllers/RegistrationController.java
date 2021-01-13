c8a303faf9b0ab2a30270c5a08b8d83c13c3e07d
doomer888 <doomer888@protonmail.com>
2021-01-13 14:31:30 +0300

    some files

--- src/main/java/slayer404/web4/controllers/RegistrationController.java
+++ src/main/java/slayer404/web4/controllers/RegistrationController.java
@@ -4,14 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import slayer404.web4.model.User;
 import slayer404.web4.repository.UserRepository;
-
-import java.util.Map;
 
 @RestController
 public class RegistrationController {
